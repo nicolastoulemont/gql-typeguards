5e6bcdf7aaa180e91bf41160ac70bf882eef4db4
ntoulemont <n.toulemont@gmail.com>
2021-08-31 16:24:03 +0200

    update dept and added isEitherTypesInTuple and isNotTypesInTuple filtering fns

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,6 @@ export function isType<
   return result?.__typename === typename;
 }
 
-export function isTypeInTuple<
-  ResultItem extends GraphQLResult,
-  Typename extends ValueOfTypename<ResultItem>
->(
-  typename: Typename
-): (o: ResultItem) => o is Extract<ResultItem, { __typename: Typename }> {
-  return function(
-    resultItem: ResultItem
-  ): resultItem is Extract<ResultItem, { __typename: Typename }> {
-    return isType(resultItem, typename);
-  };
-}
-
 export function isEither<
   Result extends GraphQLResult,
   Typename extends ValueOfTypename<Result>,
@@ -46,4 +33,55 @@ export function isNot<
 ): result is Exclude<Result, { __typename: typeof typenames[number] }> {
   const types = typenames?.filter(type => isType(result, type));
   return types ? types.length === 0 : false;
+}
+
+export function isTypeInTuple<
+  ResultItem extends GraphQLResult,
+  Typename extends ValueOfTypename<ResultItem>
+>(
+  typename: Typename
+): (o: ResultItem) => o is Extract<ResultItem, { __typename: Typename }> {
+  return function(
+    resultItem: ResultItem
+  ): resultItem is Extract<ResultItem, { __typename: Typename }> {
+    return isType(resultItem, typename);
+  };
+}
+
+export function isEitherTypesInTuple<
+  ResultItem extends GraphQLResult,
+  Typename extends ValueOfTypename<ResultItem>,
+  PossibleTypes extends Array<Typename>
+>(
+  typenames: PossibleTypes
+): (
+  o: ResultItem
+) => o is Extract<ResultItem, { __typename: typeof typenames[number] }> {
+  return function(
+    resultItem: ResultItem
+  ): resultItem is Extract<
+    ResultItem,
+    { __typename: typeof typenames[number] }
+  > {
+    return isEither(resultItem, typenames);
+  };
+}
+
+export function isNotTypesInTuple<
+  ResultItem extends GraphQLResult,
+  Typename extends ValueOfTypename<ResultItem>,
+  ExcludedTypes extends Array<Typename>
+>(
+  typenames: ExcludedTypes
+): (
+  o: ResultItem
+) => o is Exclude<ResultItem, { __typename: typeof typenames[number] }> {
+  return function(
+    resultItem: ResultItem
+  ): resultItem is Exclude<
+    ResultItem,
+    { __typename: typeof typenames[number] }
+  > {
+    return isNot(resultItem, typenames);
+  };
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,11 @@
-import { isType, isEither, isNot, isTypeInTuple } from '../src';
+import {
+  isType,
+  isEither,
+  isNot,
+  isTypeInTuple,
+  isEitherTypesInTuple,
+  isNotTypesInTuple,
+} from '../src';
 
 describe('Testing typename related helpers', () => {
   const one = {
@@ -7,12 +14,16 @@ describe('Testing typename related helpers', () => {
   const two = {
     __typename: 'DeletedUser',
   } as const;
+  const three = {
+    __typename: 'InactiveUser',
+  } as const;
 
   const userOrDeletedUserUnion = {
     __typename: 'User',
   } as { __typename: 'User' } | { __typename: 'DeletedUser' };
 
   const userOrDeletedArray = [one, two];
+  const userOrDeletedOrInactiveArray = [one, two, three];
 
   it('isType', () => {
     expect(isType(one, 'User')).toBe(true);
@@ -45,5 +56,46 @@ describe('Testing typename related helpers', () => {
     expect(user.length).toBe(1);
     expect(deletedUser.length).toBe(1);
     expect(bannedUser.length).toBe(0);
+  });
+  it('isEitherTypesInTuple', () => {
+    const userAndDeletedUser = userOrDeletedOrInactiveArray.filter(
+      isEitherTypesInTuple(['User', 'DeletedUser'])
+    );
+    const deletedUser = userOrDeletedOrInactiveArray.filter(
+      isEitherTypesInTuple(['DeletedUser'])
+    );
+    const user = userOrDeletedOrInactiveArray.filter(
+      isEitherTypesInTuple(['User'])
+    );
+
+    const bannedUser = userOrDeletedOrInactiveArray.filter(
+      // @ts-expect-error
+      isEitherTypesInTuple(['BannedUser'])
+    );
+    expect(userAndDeletedUser.length).toBe(2);
+    expect(user.length).toBe(1);
+    expect(deletedUser.length).toBe(1);
+    expect(bannedUser.length).toBe(0);
+  });
+
+  it('isNotTypesInTuple', () => {
+    const notUserAndDeletedUser = userOrDeletedOrInactiveArray.filter(
+      isNotTypesInTuple(['User', 'DeletedUser'])
+    );
+
+    const notDeletedUser = userOrDeletedOrInactiveArray.filter(
+      isNotTypesInTuple(['DeletedUser'])
+    );
+    const notUser = userOrDeletedOrInactiveArray.filter(
+      isNotTypesInTuple(['User'])
+    );
+    const bannedUser = userOrDeletedOrInactiveArray.filter(
+      // @ts-expect-error
+      isNotTypesInTuple(['BannedUser'])
+    );
+    expect(notUserAndDeletedUser.length).toBe(1);
+    expect(notUser.length).toBe(2);
+    expect(notDeletedUser.length).toBe(2);
+    expect(bannedUser.length).toBe(3);
   });
 });
